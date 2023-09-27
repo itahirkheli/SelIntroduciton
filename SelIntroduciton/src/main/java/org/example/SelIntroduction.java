42fe4e729ef4e43500b784ae0a6e2248ab4c060a
Ishtiaq Khan <Ishtiaq.khan@gmail.com>
2023-09-27 12:07:05 +0100

    Add SelIntroducitn project to git

---
+++ SelIntroduciton/src/main/java/org/example/SelIntroduction.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SelIntroduction {
+    public static void main(String[] args) {
+
+        //WebDriver driver = new ChromeDriver();
+
+        //WebDriver driver = new FirefoxDriver();
+
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("https://www.westernunionshop.com/v3/index.php");
+
+        //driver.getTitle();
+
+        System.out.println(driver.getTitle());
+
+        System.out.println(driver.getCurrentUrl());
+
+        //driver.quit();
+
+       // driver.close();
+    }
+}
