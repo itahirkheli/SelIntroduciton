fadd4f2fca3ce53ff0e4d2c7be08c5134246a38c
Ishtiaq Khan <Ishtiaq.khan@gmail.com>
2023-10-05 20:51:50 +0100

    Merge remote-tracking branch 'github/master'

--- SelIntroduciton/src/main/java/org/example/Locators.java
+++ SelIntroduciton/src/main/java/org/example/Locators.java
@@ -19,7 +19,11 @@ public class Locators {
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
         driver.findElement(By.linkText("Forgot your password?")).click();
-
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("rahul");
 
         driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("password123");
@@ -27,6 +31,25 @@ public class Locators {
         driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("07802639715");
 
         driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+
+        System.out.println(driver.findElement(By.xpath("//form/h2")).getText());
+
+        System.out.println(driver.findElement(By.xpath("//form/input[3]")).getLocation());
+
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+
+        driver.findElement(By.id("chkboxOne")).click();
+
+        driver.findElement(By.xpath("//button[@class='submit']")).click();
+
+        driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+
+
+
 
     }
 }
